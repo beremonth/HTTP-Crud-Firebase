68075df3a4b3b3a0881e08e8310dd2551a121f73
beremonth <beremonth@hotmail.com>
2019-12-21 14:11:51 -0600

    Guardamos la respuesta del servicio obtenerHeroes (objeto javascript) y lo almacenamos en propiedad miHeroeModel y mostramos en tabla html

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { HeroeModel } from '../../models/heroe.model';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit
+{
+  
+  miHeroeModel: HeroeModel[] = [];
 
-  constructor( private HeroesService: HeroesService ) { }
+  constructor( private HeroesService: HeroesService ) { } // end constructor
 
   ngOnInit()
   {
     this.HeroesService.obtenerHeroes()
-      .subscribe(resp =>
-      { 
-        console.log( resp );
-      });
-  }
+      .subscribe( resp => this.miHeroeModel = resp );
+  } // end ngOnInit
 
-}
+} // end class HeroesComponent
